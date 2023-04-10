ae3dd8fb4cfc5f5532af5b6a2b7832d5499667da
Juan Martin Zabala Ochoa <juanmarzabala8a@gmail.com>
2023-04-10 11:58:15 -0500

    Added emojis for non apple devices.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,34 @@
 <script lang="ts">
-
-  import { onMount } from 'svelte';
+  import { onMount, tick } from "svelte";
+  import twemoji from "twemoji";
   import "../app.css";
 
   let showAppleEmoji = false;
 
-  onMount(() => {
+  onMount(async () => {
     function isAppleDevice() {
       return /iPad|iPhone|iPod|Macintosh/.test(navigator.userAgent);
     }
     showAppleEmoji = isAppleDevice();
+
+    await tick(); // Wait for the next DOM update
+
+    const elements = document.querySelectorAll(".emoji");
+    elements.forEach(parseEmoji);
   });
 
+  // Add this function to parse the emoji
+  function parseEmoji(element) {
+    if (!showAppleEmoji) {
+      element.innerHTML = twemoji.parse(element.innerHTML, {
+        attributes: () => {
+          return {
+            style: "display:inline;vertical-align:middle;height:1em;width:1em;",
+          };
+        },
+      });
+    }
+  }
 </script>
 
 <div class="flex justify-center items-center h-full">
@@ -19,10 +36,8 @@
     <span class="text-xxxs">SM</span>Happ ;)
   </p>
   <p class="animate-soonfade text-center text-5xl text-white font-main font-semibold absolute px-8 md:px-16 lg:px-24 xl:px-32">
-    {#if showAppleEmoji}
-      🎉
-    {/if}
+    <span class="emoji">🎉</span>
     Your go-to event planner is almost here!
-    <span class="">{#if showAppleEmoji}📲{:else}{/if}</span>
+    <span class="emoji">📲</span>
   </p>
 </div>
