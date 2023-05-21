f10f57be6597cdc8ee3755b06ba5c12126fcbe5e
Juan Martin Zabala Ochoa <juanmarzabala8a@gmail.com>
2023-05-21 17:20:18 -0500

    fixed privacy policy and terms of use

--- src/routes/legal/privacy-policy/+page.svelte
+++ src/routes/legal/privacy-policy/+page.svelte
@@ -6,43 +6,66 @@
   <div class="mx-auto max-w-3xl p-6">
     <h1 class="text-3xl sm:text-4xl font-semibold pb-6">Privacy Policy for Happ</h1>
   
-    <p class="pb-4">Welcome to Happ - the exclusive event management platform designed with your privacy in mind. We are committed to protecting your personal information and your right to privacy. This Privacy Policy explains what information we collect from you, how we use it, and how we uphold your rights in respect to this information. By using our app, you are agreeing to the terms outlined in this policy. Please read it carefully.</p>
+    <h2 class="text-xl sm:text-2xl font-semibold pb-1">Introduction</h2>
+    <p class="pb-4">Welcome to Happ - the exclusive event platform designed with your privacy in mind. We are committed to protecting your personal information and your right to privacy. This Privacy Policy explains what information we collect from you, how we use it, the lawful basis of it, and how we uphold your rights in respect to this information. By using our app, you are agreeing to the terms outlined in this policy. Please read it carefully.</p>
   
     <h2 class="text-xl sm:text-2xl font-semibold pb-1">Information We Collect and How We Use It</h2>
     <p class="pb-4">
-      When you sign in to Happ using third-party services, such as Google and Apple, your authentication is managed through Firebase. We collect and store your name, email address, and profile picture (if provided). This information, along with a unique user ID and an automatically assigned username, is securely stored on our servers. This data collection is necessary for the performance of the contract between you and Happ.<br><br>
-      Happ allows you to create and manage exclusive, invitation-only events. For this purpose, we collect and store event-related details such as the event name, pictures, description, location, address, date, time, and the user relationships to the event (guests, hosts, and creator). This information is used to provide event details to guests and hosts, differentiate between guests and hosts, and facilitate event management.<br><br>
-      When you accept an invitation to an event as a guest, you will receive a "Pass" containing your name, username, profile picture, and a unique QR code. This pass serves as a means of identification and verification at the event, allowing hosts to confirm your attendance.<br><br>
-
+      When you sign in to Happ using third-party services, such as Google and Apple, your authentication is managed through Firebase, a development platform owned by Google. We collect and store your name, email address, profile picture (if provided, otherwise one is assigned by us), and a unique identifier provided by Firebase. This information, along with an additional unique user identifier assigned by Happ for user identification within our systems, and an automatically assigned username, are securely stored on our servers. Additionally, with your consent, we collect and store a device identifier for the purpose of sending push notifications. You can learn more about how Firebase uses your information in <a href="https://firebase.google.com/support/privacy" target="_blank" class="text-blue-700">Firebase's Privacy Policy</a>. The lawful basis for this processing is the performance of the contract between you and Happ, as it is necessary for us to provide you the services you have signed up for.<br><br>
+      Happ allows you to create and manage exclusive, invitation-only events. For this purpose, we collect and store event-related details such as the event name, pictures, description, location, address, date, time, and the user relationships to the event (guests, hosts, creator, and invited but not confirmed users). This information is used to provide event details to guests and hosts, differentiate between guests and hosts, and facilitate event management. The lawful basis for this processing is our legitimate interest in providing a comprehensive and effective event management platform.<br><br>
+      When you accept an invitation to an event as a guest, you will receive a "Pass" containing the event name, your name, username, profile picture, and a unique QR code. This pass primarily serves as a means of verification at the event, ensuring that you have been legitimately invited. The processing of this data is justified by our legitimate interest in ensuring the security and proper execution of events.<br><br>
       The QR code on your pass is unique and encrypted, adding a layer of security to your data. Event hosts can scan this QR code to verify your attendance, but they are not able to decrypt it or access any additional information from it.<br><br>
-
-      With your explicit consent, we may collect your location data. This data is used to facilitate finding a location for an event you are hosting.<br><br>
-      Our 'Friend' feature enables you to add or remove friends. The related data, as well as invitations sent by your friends, will be visible in your feed. This feature is provided based on our legitimate interest in enhancing user experience.<br><br>
-      Happ sends push notifications to keep you updated about various activities, including receiving an invitation, leaving a confirmed event, an event's cancellation, friend requests, updates to event details, acceptance of your invitations, and event reminders. These notifications are sent based on our legitimate interest in keeping you informed about your activities and updates on Happ.
+      With your explicit consent, we may collect your location data. This data is not saved on our servers unless the event's location data is your location and it is used to facilitate finding a location for an event. Additionally, when you choose to upload photos as part of the event details, we collect and store these images. The legal basis for this data collection is your consent.<br><br>
+      Our "Friendship" feature allows you to establish relationships with other users on the platform. No additional personal data is collected through this feature beyond what is already outlined in this policy. The visibility of your existing personal data, such as your name, username, and profile picture, is standard across our platform and is visible to all users.<br><br>
+      Happ sends push notifications to keep you updated about various activities, including receiving an invitation, leaving a confirmed event, an event's cancellation, friend requests, updates to event details, acceptance of your invitations, and event reminders. These notifications are facilitated by a device identifier, which we collect and store with your consent of receiving notifications. These notifications are sent based on our legitimate interest in keeping you informed about your activities and updates on Happ.
     </p>
+
+    <h2 class="text-xl sm:text-2xl font-semibold pb-1">Data Breach Notification</h2>
+    <p class="pb-4">In the unlikely event of a data breach, we will notify affected users via email within 72 hours of first having become aware of the breach. We will also take all necessary steps to minimize the impact of the breach and prevent future breaches.</p>
     
     <h2 class="text-xl sm:text-2xl font-semibold pb-1">Consent</h2>
-    <p class="pb-4">When you register for an account with Happ, you will be asked to check a box indicating that you agree to our <a href="https://www.happ.rsvp/legal/terms-of-use" class="text-blue-700">terms of use</a>, which include this Privacy Policy. Your agreement represents your informed consent to our data collection and use practices.</p>
+    <p class="">
+      Before you register for an account with Happ, you will be asked to check a box indicating that you have read, understood, and agree to our <a href="https://www.happ.rsvp/legal/terms-of-use" target="_blank" class="text-blue-700">terms of use</a>, which include this Privacy Policy. This explicit agreement represents your informed consent to our data collection and use practices.<br><br>
+
+      During the use of Happ, we may ask for your consent to collect specific types of data. This includes:<br><br>
+    </p>
+    <ul class="list-disc marker:text-black px-6">
+      <li class="pb-1">Your location data to facilitate finding an event location. This data is not saved on our servers unless the event's location data is your location.</li>
+      <li class="pb-1">Your device identifier for the purpose of sending you push notifications.</li>
+      <li>Access to your device's photo gallery, if you choose to upload photos as part of the event details.<br><br></li>
+    </ul>
+    <p class="pb-4">
+      You have the right to withdraw your consent at any time. If you wish to withdraw your consent, you can do so by changing the relevant settings on your device or contacting us at juanmarzabala8a@gmail.com.
+    </p>
   
     <h2 class="text-xl sm:text-2xl font-semibold pb-1">Data Retention and Deletion</h2>
-    <p class="pb-4">We retain your data indefinitely until you delete your account. Upon account deletion, your data is immediately and permanently deleted from our servers. We do not maintain backups of user data after account deletion.</p>
+    <p class="pb-4">
+      We retain your data for as long as necessary until you delete your account. Upon account deletion, all of your data is permanently deleted from our servers. This includes data directly associated with your user account such as your name, username, email address, profile picture, and unique identifiers.<br><br>
+      However, please note that the deletion of the relationship between your account and any event, as well as the deletion of your device identifier, may be slightly delayed depending on the server's workload.<br><br>
+      If you are the creator of an event and delete your account, the creator status will pass to another host if one exists. If there are no other hosts for the event, the event will be automatically cancelled and all information related to that event, including user relationships with the event (guests, hosts, creator, and invited but not confirmed users), will be deleted.<br><br>
+      Additionally, when an event is deleted or 7 days after the event date has passed, all event information, including user relationships with the event (guests, hosts, creator, and invited but not confirmed users), is also permanently deleted.<br><br>
+      Please be aware that these deletion processes are irreversible, and we do not maintain backups of user data after account deletion.
+    </p>
     
     <h2 class="text-xl sm:text-2xl font-semibold pb-1">User Rights</h2>
-    <p class="pb-4">You have the right to access, correct, or delete your personal data. We will comply with valid requests to exercise your data subject rights in accordance with the applicable laws in your region. To exercise these rights, contact us at juanmarzabala8a@gmail.com</p>
+    <p class="pb-4">You have the right to access, correct, or delete your personal data. We will comply with valid requests to exercise your data subject rights in accordance with the applicable laws in your region. To exercise these rights, contact us at juanmarzabala8a@gmail.com.</p>
   
-    <h2 class="text-xl sm:text-2xl font-semibold pb-1">Children’s Data</h2>
-    <p class="pb-4">Happ does not knowingly collect data from children under the age of 13. If we discover that we have inadvertently collected data from a child under the age of 13, we will immediately delete this information.</p>
+    <h2 class="text-xl sm:text-2xl font-semibold pb-1">Children’s Privacy</h2>
+    <p class="pb-4">
+      We do not knowingly collect or solicit personal information from anyone under the age of 13. If you are under 13, please do not attempt to register for the Services or send any personal information about yourself to us. If we learn that we have collected personal information from a child under age 13, we will delete that information as quickly as possible. If you believe that a child under 13 may have provided us personal information, please contact us at juanmarzabala8a@gmail.com.<br><br>
+      In the event that we learn that we have collected personal information from a child under the age of consent in their country of residence outside the United States, we will delete that information. If you believe that a child under the age of consent in their country of residence has provided us with personal information, please contact us and we will take steps to delete such information and terminate the child's account.
+    </p>
   
     <h2 class="text-xl sm:text-2xl font-semibold pb-1">Security</h2>
-    <p class="pb-4">We use industry-standard encryption methods to protect your personal data and unique QR codes. Our servers are secured and monitored regularly to prevent unauthorized access and ensure data security.</p>
+    <p class="pb-4">We utilize encryption methods specifically for the protection of your unique QR codes. Furthermore, all data within Happ is safeguarded as our servers are both secured and monitored regularly to preempt unauthorized access and maintain overall data security.</p>
   
     <h2 class="text-xl sm:text-2xl font-semibold pb-1">Data Sharing and Transfers</h2>
-    <p class="pb-4">Your data is not sold or shared with any third-party services, except <a href="https://firebase.google.com/support/privacy" class="text-blue-700">Firebase</a> which handles user authentication and push notifications. When transferring your data internationally, we comply with all data protection laws and ensure your data remains secure during these transfers through the use of standard contractual clauses.</p>
+    <p class="pb-4">Your data is not sold or shared with any third-party services, except Firebase which handles user authentication and push notifications. We use anonymized analytics data for improving our app and understanding user behavior within the app. Please note that your personal data may be transferred to, and processed in, the United States where our servers are located. When transferring your data, we comply with applicable data protection laws and ensure your data remains secure during these transfers.</p>
   
     <h2 class="text-xl sm:text-2xl font-semibold pb-1">Changes to This Privacy Policy</h2>
     <p class="pb-6">We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page and, where appropriate, notifying you via email or within our app.</p>
 
     <h2 class="text-xl sm:text-2xl font-semibold pb-1">Contact Us</h2>
-    <p class="pb-6">If you have any questions about this Privacy Policy, please contact us at juanmarzabala8a@gmail.com</p>
+    <p class="pb-6">If you have any questions about this Privacy Policy, please contact us at juanmarzabala8a@gmail.com.</p>
   </div>
 </body>
--- src/routes/legal/terms-of-use/+page.svelte
+++ src/routes/legal/terms-of-use/+page.svelte
@@ -7,33 +7,57 @@
     <h1 class="text-3xl sm:text-4xl font-semibold pb-6">Terms of Use for Happ</h1>
   
     <h2 class="text-xl sm:text-2xl font-semibold pb-1">1. Agreement to Terms</h2>
-    <p class="pb-4">By accessing and using Happ, you acknowledge that you have read, understood, and agree to be bound by these Terms of Use and our Privacy Policy, found at <a href="https://www.happ.rsvp/legal/privacy-policy" class="text-blue-700">https://www.happ.rsvp/legal/privacy-policy</a>. The Privacy Policy highlights key points about the data that we collect, how we use it, and your rights regarding that information. If you do not agree to these Terms, do not access or use Happ.</p>
+    <p class="pb-4">By accessing and using Happ, you acknowledge that you have read, understood, and agree to be bound by these Terms of Use and our Privacy Policy, found at <a href="https://www.happ.rsvp/legal/privacy-policy" target="_blank" class="text-blue-700">https://www.happ.rsvp/legal/privacy-policy</a>. The Privacy Policy highlights key points about the data that we collect, how we use it, and your rights regarding that information. If you do not agree to these Terms, do not access or use Happ.</p>
   
     <h2 class="text-xl sm:text-2xl font-semibold pb-1">2. Changes to Terms</h2>
-    <p class="pb-4">We may revise and update these Terms from time to time. We will strive to notify you directly of any substantial changes via email or through in-app notifications, but we also recommend that you periodically review these Terms for any changes.</p>
+    <p class="pb-4">We may revise and update these Terms from time to time. We will strive to notify you directly of any substantial changes via email or within our app, but we also recommend that you periodically review these Terms for any changes.</p>
     
     <h2 class="text-xl sm:text-2xl font-semibold pb-1">3. Intellectual Property Rights</h2>
     <p class="pb-4">The Happ app and its entire contents, features, and functionality (including user interface, graphics, and designs) are owned by us, our licensors, or other providers of such material and are protected by copyright, trademark, and other intellectual property rights. Conversely, you retain all rights to the content you create and share on Happ.</p>
-  
-    <h2 class="text-xl sm:text-2xl font-semibold pb-1">4. User Content</h2>
+
+    <h2 class="text-xl sm:text-2xl font-semibold pb-1">4. Respect the Copyright of Others</h2>
+    <p class="">
+      At Happ, we respect everyone's right to their own work and expect our users to do the same. In accordance with the Digital Millennium Copyright Act (DMCA), Happ will respond expeditiously to claims of copyright infringement committed using the Happ app that are reported to us. If you think someone is using your copyrighted material on our app without permission, you can let us know, and we'll look into it right away. Here's how to do that:<br><br>
+    </p>
+    <ul class="list-decimal marker:text-black px-6">
+      <li class="pb-1">Tell us which work of yours is being used without permission. If there are more than one, you can give us a list.</li>
+      <li class="pb-1">Show us where on the Happ app you found the material that you think is yours. If possible, give us the URL or other information that can help us find it.</li>
+      <li class="pb-1">Give us a way to get in touch with you, like your address, phone number, and email.</li>
+      <!-- <li class="pb-1">
+        Include these two sentences in your notice: "I believe in good faith that the use of the copyrighted material I am complaining about is not allowed by the copyright owner, its agent, or the law. I confirm that the information in this notice is accurate and swear under penalty of perjury that I am the copyright owner or am allowed to act on behalf of the copyright owner."
+      </li> -->
+      <li class="pb-1">
+        Include these two sentences in your notice:
+        <ul class="list-disc marker:text-black px-6">
+          <li class="pb-1">I have a good faith belief that the use of the copyrighted material I am complaining about is not authorized by the copyright owner, its agent, or the law.</li>
+          <li class="pb-1">The information in this notice is accurate, and under penalty of perjury, I am the copyright owner or am authorized to act on behalf of the owner of an exclusive right that is allegedly infringed.</li>
+        </ul>
+      </li>
+      <li>Put your full name at the end of the notice as your 'signature'.<br><br></li>
+    </ul>
     <p class="pb-4">
-      When you submit content to Happ, you grant us a non-exclusive, royalty-free, transferable, sub-licensable, worldwide license to use, store, display, reproduce, modify, create derivative works, perform, and distribute your content on Happ solely for the purposes of operating, developing, providing, and using the Happ app. This license continues even if you stop using our app, though you may delete your content or account, thereby ending our license for that specific content.<br><br>
+      Send this notice, with all the information, to juanmarzabala8a@gmail.com.
+    </p>
+  
+    <h2 class="text-xl sm:text-2xl font-semibold pb-1">5. User Content</h2>
+    <p class="pb-4">
+      When you submit content to Happ, you retain ownership of your content. However, by submitting content to happ, you grant us a non-exclusive, royalty-free, transferable, sub-licensable, worldwide license to use, store, display, reproduce, modify, create derivative works, perform, and distribute your content on Happ solely for the purposes of operating, developing, providing, and using the Happ app. This license continues even if you stop using our app, though you may delete your content or account, thereby ending our license for that specific content.<br><br>
       You are responsible for the content you submit to Happ. It must be accurate, non-misleading, and not infringe upon any rights of any person. Your content must not contain viruses, malicious code, unsolicited promotions, private information of third parties, infringing material, defamatory statements, obscene or offensive material, harmful or threatening content, any material that is intended to harass, harm, distress, or discriminate against others, graphic violent content, pornography, sexually explicit material, or nudity.
     </p>
     
-    <h2 class="text-xl sm:text-2xl font-semibold pb-1">5. Termination</h2>
+    <h2 class="text-xl sm:text-2xl font-semibold pb-1">6. Termination</h2>
     <p class="pb-4">We reserve the right to terminate or suspend your access to Happ for any violation of these Terms, including misuse of other user's content, unauthorised access to other user's account, or any other actions that may harm Happ or its users. We will strive to notify you directly in case of such a termination or suspension.</p>
   
-    <h2 class="text-xl sm:text-2xl font-semibold pb-1">6. Limitation of Liability</h2>
+    <h2 class="text-xl sm:text-2xl font-semibold pb-1">7. Limitation of Liability</h2>
     <p class="pb-4">To the maximum extent permitted by law, in no event shall Happ, its directors, employees, partners, agents, suppliers, or affiliates, be liable for any indirect, incidental, special, consequential, or punitive damages. Please note that this clause applies 'to the extent permitted by applicable law'.</p>
   
-    <h2 class="text-xl sm:text-2xl font-semibold pb-1">7. Governing Law</h2>
+    <h2 class="text-xl sm:text-2xl font-semibold pb-1">8. Governing Law</h2>
     <p class="pb-4">These Terms shall be governed and construed in accordance with the laws of the state of Delaware, United States. This provision will be clarified upon finalization of the company's location.</p>
   
-    <h2 class="text-xl sm:text-2xl font-semibold pb-1">8. Dispute Resolution</h2>
-    <p class="pb-4">Any disputes or claims relating to these Terms or your use of Happ will be resolved through negotiation, and if necessary, mediation or arbitration, before resorting to litigation.</p>
+    <h2 class="text-xl sm:text-2xl font-semibold pb-1">9. Dispute Resolution</h2>
+    <p class="pb-4">Any disputes or claims relating to these Terms or your use of Happ will be resolved through negotiation, and if necessary, mediation or arbitration, before resorting to litigation. The specific process and location for any arbitration will be determined in accordance with the laws of the state of Delaware, United States.</p>
   
-    <h2 class="text-xl sm:text-2xl font-semibold pb-1">9. Contact Us</h2>
+    <h2 class="text-xl sm:text-2xl font-semibold pb-1">10. Contact Us</h2>
     <p class="pb-6">If you have any questions or concerns about these Terms, or about your personal data, please don't hesitate to contact us at juanmarzabala8a@gmail.com. Whether your inquiry is related to general terms or specific data protection issues, we're here to assist you.</p>
   
     <p>Please review these Terms and Conditions carefully and check them periodically for changes. Your continued use of Happ signifies your acceptance of the updated Terms.</p>
