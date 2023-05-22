453ec9a86d68ffdd9d65805eefb1ebdcfb0432d1
Juan Martin Zabala Ochoa <juanmarzabala8a@gmail.com>
2023-05-22 14:28:56 -0500

    added contact-us page

---
+++ src/routes/contact-us/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import "../../app.css";
+</script>
+<body class="bg-black m-2 h-screen">
+  <div class="flex flex-col justify-center items-center h-full">
+    <p class="pb-5 text-5xl text-white font-main font-semibold">
+      Need a hand with our app?
+    </p>
+    <p class="text-white font-main font-semibold">
+      Drop us a line at: juanmarzabala8a@gmail.com
+    </p>
+  </div>
+</body>
