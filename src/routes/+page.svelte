ed95238a486c81ad80747bbd63942085ec7bd81d
Juan Martin Zabala Ochoa <juanmarzabala8a@gmail.com>
2024-01-07 19:45:53 -0500

    small changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,14 +17,8 @@
       <div class="h-10"/>
       <div class="flex">
         <div class="w-9 h-9 flex items-center justify-center">
-          <a href="https://apps.apple.com/app/id6449261227" target="_blank">
+          <a href="https://apps.apple.com/us/app/uya-r/id6474634122" target="_blank">
             <svg class="hover:cursor-pointer w-8 h-8 transition-transform duration-350 ease-in-out hover:scale-110" viewBox="0 0 24 24"><path fill="white" d="M17.05 20.28c-.98.95-2.05.8-3.08.35c-1.09-.46-2.09-.48-3.24 0c-1.44.62-2.2.44-3.06-.35C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.8c1.18-.24 2.31-.93 3.57-.84c1.51.12 2.65.72 3.4 1.8c-3.12 1.87-2.38 5.98.48 7.13c-.57 1.5-1.31 2.99-2.54 4.09l.01-.01zM12.03 7.25c-.15-2.23 1.66-4.07 3.74-4.25c.29 2.58-2.34 4.5-3.74 4.25z"/></svg>
-          </a>
-        </div>
-        <div class="w-8"/>
-        <div class="w-9 h-9 flex items-center justify-center">
-          <a href="https://play.google.com/store/apps/details?id=rsvp.happ.happ" target="_blank">
-            <svg class="hover:cursor-pointer w-7 h-7 transition-transform duration-350 ease-in-out hover:hover:scale-110" viewBox="0 0 24 24"><path fill="white" d="M3 20.5v-17c0-.59.34-1.11.84-1.35L13.69 12l-9.85 9.85c-.5-.25-.84-.76-.84-1.35m13.81-5.38L6.05 21.34l8.49-8.49l2.27 2.27m3.35-4.31c.34.27.59.69.59 1.19s-.22.9-.57 1.18l-2.29 1.32l-2.5-2.5l2.5-2.5l2.27 1.31M6.05 2.66l10.76 6.22l-2.27 2.27l-8.49-8.49Z"/></svg>
           </a>
         </div>
       </div>
